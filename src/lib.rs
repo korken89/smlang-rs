0583f911834066a6084cf88b050c108143aac9a2
Emil Fresk <emil.fresk@gmail.com>
2019-04-28 12:35:38 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,208 +1,21 @@
 extern crate proc_macro;
 extern crate syn;
 
+mod parser;
+
+//use parser::*;
+
 use quote::{quote, TokenStreamExt};
-use syn::parse::{Parse, ParseStream, Result};
-use syn::{
-    bracketed,
-    token::{self, Colon2},
-};
-use syn::{parse_macro_input, punctuated::Punctuated, Ident, Path, PathSegment, Token};
+use syn::parse_macro_input;
 
-use std::collections::HashMap;
-
-#[derive(Debug)]
-struct StateMachine {
-    pub transitions: Vec<StateTransition>,
-}
-
-impl StateMachine {
-    fn new() -> Self {
-        StateMachine {
-            transitions: Vec::new(),
-        }
-    }
-
-    fn add_transition(&mut self, transition: StateTransition) {
-        self.transitions.push(transition);
-    }
-}
-
-#[derive(Debug)]
-struct EventMapping {
-    pub event: Ident,
-    pub guard: Option<Ident>,
-    pub action: Option<Ident>,
-    pub out_state: Ident,
-}
-
-fn eventmapping_to_tokens(em: &EventMapping) {
-    let event = &em.event;
-
-    let event = quote! { Events::#event };
-
-    println!("Ev: {:?}", event.to_string());
-}
-
-#[derive(Debug)]
-struct ParsedStateMachine {
-    pub states: HashMap<String, Ident>,
-    pub events: HashMap<String, Ident>,
-    pub states_events_mapping: HashMap<String, HashMap<String, EventMapping>>,
-}
-
-impl ParsedStateMachine {
-    fn new(sm: StateMachine) -> Self {
-        // Check the initial state definition
-        let num_start: u32 = sm
-            .transitions
-            .iter()
-            .map(|sm| if sm.start { 1 } else { 0 })
-            .sum();
-
-        if num_start == 0 {
-            panic!("No starting state defined, indicate the starting state with a *");
-        } else if num_start > 1 {
-            panic!("More than one starting state defined, remove duplicates");
-        }
-
-        let mut states = HashMap::new();
-        let mut events = HashMap::new();
-        let mut states_events_mapping = HashMap::<String, HashMap<String, EventMapping>>::new();
-
-        for transition in sm.transitions.iter() {
-            // Collect states
-            states.insert(transition.in_state.to_string(), transition.in_state.clone());
-            states.insert(
-                transition.out_state.to_string(),
-                transition.out_state.clone(),
-            );
-
-            // Collect events
-            events.insert(transition.event.to_string(), transition.event.clone());
-
-            // Setup the states to events mapping
-            states_events_mapping.insert(transition.in_state.to_string(), HashMap::new());
-        }
-
-        // Create states to event mappings
-        for transition in sm.transitions.iter() {
-            let p = states_events_mapping
-                .get_mut(&transition.in_state.to_string())
-                .unwrap();
-
-            if let None = p.get(&transition.event.to_string()) {
-                let mapping = EventMapping {
-                    event: transition.event.clone(),
-                    guard: transition.guard.clone(),
-                    action: transition.action.clone(),
-                    out_state: transition.out_state.clone(),
-                };
-
-                eventmapping_to_tokens(&mapping);
-
-                p.insert(transition.event.to_string(), mapping);
-            } else {
-                panic!("State and event combination specified multiple times, remove duplicates");
-            }
-        }
-
-        ParsedStateMachine {
-            states,
-            events,
-            states_events_mapping,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct StateTransition {
-    start: bool,
-    in_state: Ident,
-    event: Ident,
-    guard: Option<Ident>,
-    action: Option<Ident>,
-    out_state: Ident,
-}
-
-impl Parse for StateMachine {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let mut statemachine = StateMachine::new();
-
-        loop {
-            // If the last line ends with a comma this is true
-            if input.is_empty() {
-                break;
-            }
-
-            // Check for starting state definition
-            let start = if let Ok(_) = input.parse::<Token![*]>() {
-                true
-            } else {
-                false
-            };
-
-            //
-            // Parse the DSL
-            //
-            // Transition DSL: src_state + event [ guard ] / action = dst_state
-
-            // State and event
-            let in_state: Ident = input.parse()?;
-            input.parse::<Token![+]>()?;
-            let event: Ident = input.parse()?;
-
-            // Possible guard
-            let guard = if input.peek(token::Bracket) {
-                let content;
-                bracketed!(content in input);
-                let guard: Ident = content.parse()?;
-                Some(guard)
-            } else {
-                None
-            };
-
-            // Possible action
-            let action = if let Ok(_) = input.parse::<Token![/]>() {
-                let action: Ident = input.parse()?;
-                Some(action)
-            } else {
-                None
-            };
-
-            input.parse::<Token![=]>()?;
-
-            let out_state: Ident = input.parse()?;
-
-            statemachine.add_transition(StateTransition {
-                start,
-                in_state,
-                event,
-                guard,
-                action,
-                out_state,
-            });
-
-            // No comma at end of line, no more transitions
-            if input.is_empty() {
-                break;
-            }
-
-            if let Err(_) = input.parse::<Token![,]>() {
-                break;
-            };
-        }
-
-        Ok(statemachine)
-    }
-}
+// use std::collections::HashMap;
 
 #[proc_macro]
 pub fn statemachine(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the syntax into structures
-    let input = parse_macro_input!(input as StateMachine);
-    let sm = ParsedStateMachine::new(input);
-    println!("ParsedStateMachine: {:#?}", sm);
+    let input = parse_macro_input!(input as parser::StateMachine);
+    let sm = parser::ParsedStateMachine::new(input);
+    //println!("ParsedStateMachine: {:#?}", sm);
 
     // Get only the unique states
     let states = sm.states.iter().map(|(_, value)| value);
@@ -254,6 +67,20 @@ pub fn statemachine(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     };
 
     output.append_all(sm);
+
+    let a = &vec!["a", "b"];
+    let mut b = &vec![vec!["a1", "a2"], vec!["b1", "b2"]];
+    //b.clear();
+
+    let test = quote! {
+    fn test() {
+        println!(#(#a),*);
+        println!(#(#a #(#b),*),*);
+        println!(#(#(#b),*),*);
+    }
+    };
+
+    println!("Test: {:#?}", test.to_string());
 
     // Hand the output tokens back to the compiler
     output.into()
---
+++ src/parser.rs
@@ -0,0 +1,192 @@
+use quote::quote;
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{bracketed, token};
+use syn::{Ident, Token};
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct StateMachine {
+    pub transitions: Vec<StateTransition>,
+}
+
+impl StateMachine {
+    pub fn new() -> Self {
+        StateMachine {
+            transitions: Vec::new(),
+        }
+    }
+
+    pub fn add_transition(&mut self, transition: StateTransition) {
+        self.transitions.push(transition);
+    }
+}
+
+#[derive(Debug)]
+pub struct EventMapping {
+    pub event: Ident,
+    pub guard: Option<Ident>,
+    pub action: Option<Ident>,
+    pub out_state: Ident,
+}
+
+pub fn eventmapping_to_tokens(em: &EventMapping) {
+    let event = &em.event;
+
+    let event = quote! { Events::#event };
+
+    println!("Ev: {:?}", event.to_string());
+}
+
+#[derive(Debug)]
+pub struct ParsedStateMachine {
+    pub states: HashMap<String, Ident>,
+    pub events: HashMap<String, Ident>,
+    pub states_events_mapping: HashMap<String, HashMap<String, EventMapping>>,
+}
+
+impl ParsedStateMachine {
+    pub fn new(sm: StateMachine) -> Self {
+        // Check the initial state definition
+        let num_start: u32 = sm
+            .transitions
+            .iter()
+            .map(|sm| if sm.start { 1 } else { 0 })
+            .sum();
+
+        if num_start == 0 {
+            panic!("No starting state defined, indicate the starting state with a *");
+        } else if num_start > 1 {
+            panic!("More than one starting state defined, remove duplicates");
+        }
+
+        let mut states = HashMap::new();
+        let mut events = HashMap::new();
+        let mut states_events_mapping = HashMap::<String, HashMap<String, EventMapping>>::new();
+
+        for transition in sm.transitions.iter() {
+            // Collect states
+            states.insert(transition.in_state.to_string(), transition.in_state.clone());
+            states.insert(
+                transition.out_state.to_string(),
+                transition.out_state.clone(),
+            );
+
+            // Collect events
+            events.insert(transition.event.to_string(), transition.event.clone());
+
+            // Setup the states to events mapping
+            states_events_mapping.insert(transition.in_state.to_string(), HashMap::new());
+        }
+
+        // Create states to event mappings
+        for transition in sm.transitions.iter() {
+            let p = states_events_mapping
+                .get_mut(&transition.in_state.to_string())
+                .unwrap();
+
+            if let None = p.get(&transition.event.to_string()) {
+                let mapping = EventMapping {
+                    event: transition.event.clone(),
+                    guard: transition.guard.clone(),
+                    action: transition.action.clone(),
+                    out_state: transition.out_state.clone(),
+                };
+
+                eventmapping_to_tokens(&mapping);
+
+                p.insert(transition.event.to_string(), mapping);
+            } else {
+                panic!("State and event combination specified multiple times, remove duplicates");
+            }
+        }
+
+        ParsedStateMachine {
+            states,
+            events,
+            states_events_mapping,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct StateTransition {
+    start: bool,
+    in_state: Ident,
+    event: Ident,
+    guard: Option<Ident>,
+    action: Option<Ident>,
+    out_state: Ident,
+}
+
+impl Parse for StateMachine {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut statemachine = StateMachine::new();
+
+        loop {
+            // If the last line ends with a comma this is true
+            if input.is_empty() {
+                break;
+            }
+
+            // Check for starting state definition
+            let start = if let Ok(_) = input.parse::<Token![*]>() {
+                true
+            } else {
+                false
+            };
+
+            //
+            // Parse the DSL
+            //
+            // Transition DSL: src_state + event [ guard ] / action = dst_state
+
+            // State and event
+            let in_state: Ident = input.parse()?;
+            input.parse::<Token![+]>()?;
+            let event: Ident = input.parse()?;
+
+            // Possible guard
+            let guard = if input.peek(token::Bracket) {
+                let content;
+                bracketed!(content in input);
+                let guard: Ident = content.parse()?;
+                Some(guard)
+            } else {
+                None
+            };
+
+            // Possible action
+            let action = if let Ok(_) = input.parse::<Token![/]>() {
+                let action: Ident = input.parse()?;
+                Some(action)
+            } else {
+                None
+            };
+
+            input.parse::<Token![=]>()?;
+
+            let out_state: Ident = input.parse()?;
+
+            statemachine.add_transition(StateTransition {
+                start,
+                in_state,
+                event,
+                guard,
+                action,
+                out_state,
+            });
+
+            // No comma at end of line, no more transitions
+            if input.is_empty() {
+                break;
+            }
+
+            if let Err(_) = input.parse::<Token![,]>() {
+                break;
+            };
+        }
+
+        Ok(statemachine)
+    }
+}
