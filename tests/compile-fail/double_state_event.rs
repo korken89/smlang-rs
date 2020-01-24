546e859e1268881f37144f35a84221d2edecf1f3
Emil Fresk <emil.fresk@gmail.com>
2020-01-24 15:50:40 +0100

    Started adding compile fail tests

---
+++ tests/compile-fail/double_state_event.rs
@@ -0,0 +1,10 @@
+extern crate smlang;
+
+use smlang::statemachine;
+
+statemachine! {
+    *State1 + Event1 = State2,
+    State1 + Event1 = State3, //~ State and event combination specified multiple times, remove duplicates
+}
+
+fn main() {}
