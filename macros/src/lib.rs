3213c33482e32bd1b0d39c714dcf8e2e31fafd27
Emil Fresk <emil.fresk@gmail.com>
2020-05-07 10:16:35 +0200

    Diagram generation only if requested

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,6 +3,7 @@
 extern crate proc_macro;
 
 mod codegen;
+#[cfg(feature = "graphviz")]
 mod diagramgen;
 mod parser;
 
@@ -19,20 +20,16 @@ pub fn statemachine(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     match parser::ParsedStateMachine::new(input) {
         // Generate code and hand the output tokens back to the compiler
         Ok(sm) => {
-            use std::io::Write;
-
-            // Generate dot syntax for the statemachine.
-            let diagram = diagramgen::generate_diagram(&sm);
-
             #[cfg(feature = "graphviz")]
             {
+                use std::io::Write;
+
+                // Generate dot syntax for the statemachine.
+                let diagram = diagramgen::generate_diagram(&sm);
+
                 // Start the 'dot' process.
                 let mut process = std::process::Command::new("dot")
-                    .args(&[
-                        "-Tsvg",
-                        "-o",
-                        "statemachine.svg",
-                    ])
+                    .args(&["-Tsvg", "-o", "statemachine.svg"])
                     .stdin(std::process::Stdio::piped())
                     .spawn()
                     .expect("Failed to execute 'dot'. Are you sure graphviz is installed?");
@@ -45,39 +42,17 @@ pub fn statemachine(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
                 // Check the graphviz return status to see if it was successful.
                 match process.wait() {
-                    Ok(status) => if !status.success() {
-                        panic!("'dot' failed to run. Are you sure graphviz is installed?");
-                    },
-                    Err(_) => panic!("'dot' failed to run. Are you sure graphviz is installed?")
-                }
-            }
-            #[cfg(not(feature = "graphviz"))]
-            {
-                // Try open a dot file.
-                match std::fs::OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .open("statemachine.gv") {
-                        Ok(mut file) => {
-                            // Try write to the dot file.
-                            if let Err(_err) = file.write_all(diagram.as_bytes()) {
-                                // Fallback to printing to the commandline.
-                                // println!("'statemachine.dot' could not be created: {}", err);
-                                // println!("Here is the contents:");
-                                // println!("{}", diagram);
-                            }
-                        },
-                        Err(_err) => {
-                            // Fallback to printing to the commandline.
-                            // println!("'statemachine.dot' could not be created: {}", err);
-                            // println!("Here is the contents:");
-                            // println!("{}", diagram);
+                    Ok(status) => {
+                        if !status.success() {
+                            panic!("'dot' failed to run. Are you sure graphviz is installed?");
                         }
+                    }
+                    Err(_) => panic!("'dot' failed to run. Are you sure graphviz is installed?"),
                 }
             }
 
             codegen::generate_code(&sm).into()
-        },
+        }
         Err(error) => error.to_compile_error().into(),
     }
 }
