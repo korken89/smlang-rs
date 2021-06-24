5c694d06f8e10c85b52b8c65ccf5a211b77c4d01
Mathias <mk@blackbird.online>
2021-06-24 14:09:33 +0200

    Fix unit tests

--- tests/compile-fail/double_state_event.rs
+++ tests/compile-fail/double_state_event.rs
@@ -3,8 +3,10 @@ extern crate smlang;
 use smlang::statemachine;
 
 statemachine! {
-    *State1 + Event1 = State2,
-    State1 + Event1 = State3, //~ State and event combination specified multiple times, remove duplicates.
+    transitions: {
+        *State1 + Event1 = State2,
+        State1 + Event1 = State3, //~ State and event combination specified multiple times, remove duplicates.
+    }
 }
 
 fn main() {}
