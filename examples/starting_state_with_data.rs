f0ff3962833159f3a2d06afd25d53280d577ea97
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2022-02-07 08:43:48 -0800

    Added an example with a state machine that has data used in the input state, which will be used as a test.

---
+++ examples/starting_state_with_data.rs
@@ -0,0 +1,36 @@
+//! State data example
+//!
+//! An example of using state data together with an action.
+
+#![deny(missing_docs)]
+
+use smlang::statemachine;
+
+/// State data
+#[derive(PartialEq)]
+pub struct MyStateData(pub u32);
+
+statemachine! {
+    transitions: {
+        *State1(MyStateData) + Event1 = State2,
+        State2 + Event2 / action = State1,
+        // ...
+    }
+}
+
+/// Context
+pub struct Context;
+
+impl StateMachineContext for Context {
+    fn action(&mut self) -> MyStateData {
+        MyStateData(42)
+    }
+}
+
+fn main() {
+    let mut sm = StateMachine::new(Context, MyStateData(42));
+    let _ = sm.process_event(Events::Event1);
+    let result = sm.process_event(Events::Event2);
+
+    assert!(result == Ok(&States::State1(MyStateData(42))));
+}
